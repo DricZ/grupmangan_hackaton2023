6215922f46695c904fdb5ac55c410edda081cfa8
kevingunawanc14 <c14200005@john.petra.ac.id>
2023-05-13 13:46:17 +0700

    login

---
+++ head.php
@@ -0,0 +1,46 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <link rel="icon" type="image/x-icon" href="./assets/img/logo-ct.png" style="margin: -10px;">
+    <link rel="stylesheet" href="./assets/css/login.css">
+    
+    <title>
+        Anu - <?php echo $_SESSION['page'] ?>
+    </title>
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.css" rel="stylesheet">
+
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script>
+    // document.addEventListener("keydown", function(e) {
+    //   if (e.key === "F12") {
+    //       e.preventDefault();
+    //       window.location.replace("./css/");
+    //   }
+    //   if (e.ctrlKey && e.key === "p") {
+    //     e.preventDefault();
+    //   }
+    // });
+
+    // document.addEventListener('contextmenu', function(event) {
+    //   event.preventDefault();
+    // });
+    </script>
+
+    <style>
+    html{
+        scroll-behavior: smooth;
+    }
+
+    @media print {
+      * {
+        display: none !important;
+        }
+    }
+    </style>
+</head>
